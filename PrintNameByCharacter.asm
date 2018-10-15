1c9de590799a818437063ea8e256d0efd9f89bb5
Kaushik Mishra <kaushik530411@gmail.com>
2018-10-14 22:01:57 -0400

    Added code to print three more digits

--- PrintNameByCharacter.asm
+++ PrintNameByCharacter.asm
@@ -8,7 +8,16 @@ main:
 	add $a0, $a0, -64  #  Stores the value 0
 	syscall
 
-	add $a0, $a0, 2
+	add $a0, $a0, 2  #  Store the value 2
+	syscall
+
+	add $a0, $a0, 6  #  Stores the value 8
+	syscall
+
+	add $a0, $a0, -2  #  Stores the value 6
+	syscall
+
+	add $a0, $a0, -1  #Stores the value 5
 	syscall
 
 	li $v0, 10
